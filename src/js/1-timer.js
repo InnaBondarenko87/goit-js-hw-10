59ad0f7af88818f7762acc8ba5e32d4264f9b759
InnaBondarenko87 <bondarenko211287@gmail.com>
2025-04-01 22:14:21 +0200

    timer

--- src/js/1-timer.js
+++ src/js/1-timer.js
@@ -1,18 +1,18 @@
 import flatpickr from 'flatpickr';
 import 'flatpickr/dist/flatpickr.min.css';
-
 import iziToast from 'izitoast';
 import 'izitoast/dist/css/iziToast.min.css';
 
 let userSelectedDate;
+let countId;
 
 const inputEl = document.querySelector('#datetime-picker');
 const buttonEl = document.querySelector('[data-start]');
 
-days: document.querySelector('[data-days]');
-hours: document.querySelector('[data-hours]');
-minutes: document.querySelector('[data-minutes]');
-seconds: document.querySelector('[data-seconds]');
+const timerDays = document.querySelector('[data-days]');
+const timerHours = document.querySelector('[data-hours]');
+const timerMinutes = document.querySelector('[data-minutes]');
+const timerSeconds = document.querySelector('[data-seconds]');
 
 const fp = flatpickr(inputEl, {
   enableTime: true,
@@ -42,54 +42,10 @@ const fp = flatpickr(inputEl, {
 
 buttonEl.addEventListener('click', startCount);
 
-// ====================================
-// import flatpickr from "flatpickr";
-// import "flatpickr/dist/flatpickr.min.css";
-// import iziToast from "izitoast";
-// import "izitoast/dist/css/iziToast.min.css";
-
-const datetimePicker = document.querySelector('#datetime-picker');
-const startButton = document.querySelector('[data-start]');
-const daysValue = document.querySelector('[data-days]');
-const hoursValue = document.querySelector('[data-hours]');
-const minValue = document.querySelector('[data-minutes]');
-const secValue = document.querySelector('[data-seconds]');
-
-// let userSelectedDate = null;
-let countId = null;
-
-// const fp = flatpickr(datetimePicker, {
-//     enableTime: true,
-//     time_24hr: true,
-//     minuteIncrement: 1,
-//     defaultDate: new Date(),
-//     onOpen() {
-//         this.setDate(new Date());
-//     },
-//     onClose(selectedDates) {
-//         const selectedDate = selectedDates[0];
-//         const currentDate = new Date();
-//                 if (selectedDate <= currentDate) {
-//             iziToast.error({
-//                 position: "topRight",
-//                 title: "Error",
-//                 message: "Please choose a date in the future",
-//             });
-//             userSelectedDate = null;
-//             startButton.disabled = true;
-//         } else {
-//             userSelectedDate = selectedDate;
-//             startButton.disabled = false;
-//         }
-//     },
-// });
-
-// startButton.addEventListener("click", startCount);
-
 function startCount() {
   if (!userSelectedDate) return;
 
-  startButton.disabled = true;
+  buttonEl.disabled = true;
   fp.input.disabled = true;
 
   updateTimer();
@@ -110,38 +66,11 @@ function updateTimer() {
   updateTimerDisplay(convertMs(timeLeft));
 }
 
-// function updateTimerDisplay({ days, hours, minutes, seconds }) {
-//     daysValue.textContent = addLeadingZero(days);
-//     hoursValue.textContent = addLeadingZero(hours);
-//     minValue.textContent = addLeadingZero(minutes);
-//     secValue.textContent = addLeadingZero(seconds);
-// }
-
-// function addLeadingZero(value) {
-//     return String(value).padStart(2, "0");
-// }
-
-// function convertMs(ms) {
-//     const second = 1000;
-//     const minute = second * 60;
-//     const hour = minute * 60;
-//     const day = hour * 24;
-
-//     const days = Math.floor(ms / day);
-//     const hours = Math.floor((ms % day) / hour);
-//     const minutes = Math.floor(((ms % day) % hour) / minute);
-//     const seconds = Math.floor((((ms % day) % hour) % minute) / second);
-
-//     return { days, hours, minutes, seconds };
-// }
-
-// ===================================
-
 function updateTimerDisplay({ days, hours, minutes, seconds }) {
-  daysValue.textContent = addLeadingZero(days);
-  hoursValue.textContent = addLeadingZero(hours);
-  minValue.textContent = addLeadingZero(minutes);
-  secValue.textContent = addLeadingZero(seconds);
+  timerDays.textContent = addLeadingZero(days);
+  timerHours.textContent = addLeadingZero(hours);
+  timerMinutes.textContent = addLeadingZero(minutes);
+  timerSeconds.textContent = addLeadingZero(seconds);
 }
 
 function addLeadingZero(value) {
@@ -149,14 +78,19 @@ function addLeadingZero(value) {
 }
 
 function convertMs(ms) {
+  // Number of milliseconds per unit of time
   const second = 1000;
   const minute = second * 60;
   const hour = minute * 60;
   const day = hour * 24;
 
+  // Remaining days
   const days = Math.floor(ms / day);
+  // Remaining hours
   const hours = Math.floor((ms % day) / hour);
+  // Remaining minutes
   const minutes = Math.floor(((ms % day) % hour) / minute);
+  // Remaining seconds
   const seconds = Math.floor((((ms % day) % hour) % minute) / second);
 
   return { days, hours, minutes, seconds };
